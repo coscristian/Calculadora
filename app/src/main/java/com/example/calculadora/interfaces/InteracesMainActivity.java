afdd93c7ca4ede31d5aa340c45cd36174e63db62
coscristian <crisquesadaco@gmail.com>
2023-01-20 14:19:06 -0500

    Adding packages using MVP

---
+++ app/src/main/java/com/example/calculadora/interfaces/InteracesMainActivity.java
@@ -0,0 +1,15 @@
+package com.example.calculadora.interfaces;
+
+public interface InteracesMainActivity {
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+    interface Model {
+
+    }
+}
